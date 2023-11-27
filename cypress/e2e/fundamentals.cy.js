29d6b173e53b7348bce6a1f825f3866659d429b1
OkkashaAlly <steamnature006@gmail.com>
2023-11-27 08:57:12 +0300

    Fundamentals 5) Getting elements and chain assertion

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Fundamentals spec", () => {
+  it("Page title", () => {
+    cy.visit("http://localhost:3000/fundamentals");
+    // cy.get('[data-test="fundamental-title"]').contains(/Testing fundamental/i);
+    cy.get('[data-test="fundamental-title"]').should('have.text', 'Testing Fundamentals');
+  });
+});
