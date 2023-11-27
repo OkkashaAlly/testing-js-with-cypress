9269e346c9f56790431df8d68eeb2117816bca57
OkkashaAlly <steamnature006@gmail.com>
2023-11-27 09:14:19 +0300

    More on assertion, clicking an element

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,7 +1,24 @@
 describe("Fundamentals spec", () => {
   it("Page title", () => {
-    cy.visit("http://localhost:3000/fundamentals");
+    cy.visit("/fundamentals");
     // cy.get('[data-test="fundamental-title"]').contains(/Testing fundamental/i);
-    cy.get('[data-test="fundamental-title"]').should('have.text', 'Testing Fundamentals');
+    cy.get('[data-test="fundamental-title"]').should(
+      "have.text",
+      "Testing Fundamentals"
+    );
+  });
+  it("Accordion works", () => {
+    cy.visit("/fundamentals");
+    cy.contains("Your tests will exist in a describe block.").should(
+      "not.be.visible"
+    );
+    cy.get("[data-test='accordion-item-1'] div[role='button']").click();
+    cy.contains("Your tests will exist in a describe block.").should(
+      "be.visible"
+    );
+    cy.get("[data-test='accordion-item-1'] div[role='button']").click();
+    cy.contains("Your tests will exist in a describe block.").should(
+      "not.be.visible"
+    );
   });
 });
