8c3fed68e01dc35f28363f5c7bc36c371f000e51
OkkashaAlly <steamnature006@gmail.com>
2023-11-28 07:45:09 +0300

    Testing Forms Full form validation with their corresponding errors

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -4,6 +4,7 @@ describe("Forms spec", () => {
   });
   it("test subscribe form", () => {
     cy.contains(/testing forms/i);
+    cy.wait(3000);
     cy.getDataTest("subscribe-form").find("input").as("subscribe-input");
     cy.contains(/Successfully subbed: okkashaally.com/i).should("not.exist");
     cy.get("@subscribe-input").type("okkashaally.com");
@@ -12,9 +13,17 @@ describe("Forms spec", () => {
     cy.wait(3000);
     cy.contains(/Successfully subbed: okkashaally.com/i).should("not.exist");
 
-    cy.contains(/invalid: okkashaally.me/i).should("not.exist");
     cy.get("@subscribe-input").type("okkashaally.me");
+    cy.contains(/invalid email: okkashaally.me/i).should("not.exist");
     cy.getDataTest("subscribe-btn").click();
-    cy.contains(/invalid: okkashaally.me/i).should("exist");
+    cy.contains(/invalid email: okkashaally.me/i).should("exist");
+    cy.wait(3000);
+    cy.contains(/invalid email: okkashaally.me/i).should("not.exist");
+
+    cy.contains(/fail!/i).should("not.exist");
+    cy.getDataTest("subscribe-btn").click();
+    cy.contains(/fail!/i).should("exist");
+    cy.wait(3000);
+    cy.contains(/fail!/i).should("not.exist");
   });
 });
