c048311f67b083496c2ee3eee45f8271d37080a4
OkkashaAlly <steamnature006@gmail.com>
2023-11-28 07:29:39 +0300

    cy aliases and cy wait

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -4,9 +4,17 @@ describe("Forms spec", () => {
   });
   it("test subscribe form", () => {
     cy.contains(/testing forms/i);
+    cy.getDataTest("subscribe-form").find("input").as("subscribe-input");
     cy.contains(/Successfully subbed: okkashaally.com/i).should("not.exist");
-    cy.getDataTest("subscribe-form").find("input").type("okkashaally.com");
+    cy.get("@subscribe-input").type("okkashaally.com");
     cy.getDataTest("subscribe-btn").click();
     cy.contains(/Successfully subbed: okkashaally.com/i).should("exist");
+    cy.wait(3000);
+    cy.contains(/Successfully subbed: okkashaally.com/i).should("not.exist");
+
+    cy.contains(/invalid: okkashaally.me/i).should("not.exist");
+    cy.get("@subscribe-input").type("okkashaally.me");
+    cy.getDataTest("subscribe-btn").click();
+    cy.contains(/invalid: okkashaally.me/i).should("exist");
   });
 });
