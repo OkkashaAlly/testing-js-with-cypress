ada3222e2ab06ed023326dc20ffeeabd0dd4f929
OkkashaAlly <steamnature006@gmail.com>
2023-11-28 07:16:45 +0300

    Testing Forms Step 4) Form validation - success/fail

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -4,9 +4,9 @@ describe("Forms spec", () => {
   });
   it("test subscribe form", () => {
     cy.contains(/testing forms/i);
-    // cy.contains(/Successfully subbed: okkashaally.com/i).should("not.exist");
+    cy.contains(/Successfully subbed: okkashaally.com/i).should("not.exist");
     cy.getDataTest("subscribe-form").find("input").type("okkashaally.com");
     cy.getDataTest("subscribe-btn").click();
-    // cy.contains(/Successfully subbed: okkashaally.com/i).should("exist");
+    cy.contains(/Successfully subbed: okkashaally.com/i).should("exist");
   });
 });
