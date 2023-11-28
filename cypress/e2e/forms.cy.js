f867d41f01f00a629f371be7a68503f6f4bf4bb0
OkkashaAlly <steamnature006@gmail.com>
2023-11-28 06:52:19 +0300

    Testing Forms Step 1) Type Form input

--- cypress/e2e/forms.cy.js
+++ cypress/e2e/forms.cy.js
@@ -4,6 +4,6 @@ describe("Forms spec", () => {
   });
   it("test subscribe form", () => {
     cy.contains(/testing forms/i);
-    cy.getDataTest('subscribe-form').find('input')
+    cy.getDataTest('subscribe-form').find('input').type('okkashaally.me')
   });
 });
