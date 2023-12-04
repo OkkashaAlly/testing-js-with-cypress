cf3f7319e4504ff852ed1992217854584d20e450
OkkashaAlly <steamnature006@gmail.com>
2023-12-05 00:03:34 +0300

    Examples 4) Grudge list - adding items

--- cypress/e2e/examples.cy.js
+++ cypress/e2e/examples.cy.js
@@ -18,11 +18,40 @@ describe("Various Examples", () => {
     cy.getDataTest("nav-examples").click();
     cy.location("pathname").should("equal", "/examples");
   });
-  it.only("intercept request", () => {
+  it("intercept request", () => {
     cy.intercept("POST", "http://localhost:3000/examples", {
       fixture: "example.json",
     });
     cy.wait(3000);
     cy.getDataTest("post-btn").click();
+  });
+  it.only("grudge list work", () => {
+    cy.wait(3000);
+    cy.contains(/add some grudge/i);
+
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li").should("have.length", 0);
+    });
+
+    cy.getDataTest("grudge-input").within(() => {
+      cy.get("input").type("Some grudge");
+    });
+    cy.getDataTest("grudge-btn").click();
+    cy.wait(2000);
+
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li").should("have.length", 1);
+    });
+
+    cy.getDataTest("grudge-input").within(() => {
+      cy.get("input").type("number 2");
+    });
+    cy.getDataTest("grudge-btn").click();
+    cy.wait(2000);
+
+    cy.getDataTest("grudge-list").within(() => {
+      cy.get("li").should("have.length", 2);
+      cy.get("li").its(0).should("contains.text", "Some grudge");
+    });
   });
 });
